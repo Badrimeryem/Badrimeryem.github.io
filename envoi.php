da6d8defbe036a1c0ffc6dc91d7230f4dd6edb5b
Badrimeryem <80628293+Badrimeryem@users.noreply.github.com>
2021-03-18 18:38:57 +0100

    Update envoi.php

--- envoi.php
+++ envoi.php
@@ -1,41 +1,14 @@
 <?php
-error_reporting(E_ALL);
-var_dump($_POST);
-$EmailFrom = $_POST['email']; 
-$EmailTo = "badrimeryem6@gmail.com";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 
-$Nom = $_POST['nom']; 
-$Email = $_POST['email'];
-$Subject = "sujet";
-$Message = $_POST['message']; 
- 
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  echo "Error";
-  exit;
-}
- 
-// mise en form mail
-$Body = "";
-
-$Body .= "Nom: ";
-$Body .= $Nom;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Sujet: ";
-$Body .= $Sujet;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
- 
-// envoi email 
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-	
-// message success-error
 if ($success){
   echo "Votre message à bien été envoyé.";
 }
